20c18bfcc89283d35cfbee209fb09badb709addb
root <root@PORT202105-018.localdomain>
2021-07-20 09:59:00 +0200

    Fricoot

--- index.php
+++ index.php
@@ -35,19 +35,25 @@ session_start();
     <header>  
         <h1> Bienvenue sur le Quiz qui vous fait perdre du fric</h1>     
     </header>
-    <nav class="box">
-        <form class="form" method="post" action="attente.php">
-            <div>
-                <label for="pseudo">Pseudo :</label>
-                <input id="pseudo" class="champs" placeholder="Pseudo" name="pseudo" maxlength="20">
+    <nav class="box">	
+        <div class="controller__AppWrapper-sc-1m4rw0k-0 test">
+            <div class="background__Background-sc-15eg2v3-0 test1">
+                <aside class="background-shapes__CircleShape-waao7z-1 gVOCQO"></aside>
+                <aside class="background-shapes__CircleShape-waao7z-1 eanPtI"></aside>
+                <form class="form" method="post" action="attente.php">
+                    <div>
+                        <label for="pseudo">Votre pseudo :</label>
+                        <input id="pseudo" class="champs" placeholder="Pseudo" name="pseudo" maxlength="20">
+                    </div>
+                    <div>
+                        <label for="pin">Le code pin :</label>
+                        <input name="pin" id="pin" class="champs" placeholder="Code Pin" maxlength="10">
+                    </div>
+                    <div class="button" id="submit">
+                        <input name="start" type="submit" value="Valider">
+                    </div>
+                </form>
             </div>
-            <div>
-                <label for="pin">Code pin :</label>
-                <input name="pin" id="pin" class="champs" placeholder="Code Pin" maxlength="10">
-            </div>
-            <div class="button" id="submit">
-                <input name="start" type="submit" value="valider">
-            </div>
-        </form>
+        </div>    
     </nav>    
 <?php require "./templates/footer.php" ?>
