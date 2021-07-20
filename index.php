8a51a6216581b4e8cdbc6a69bc2033fbca3678da
root <root@PORT202105-018.localdomain>
2021-07-20 12:01:53 +0200

    Fricoot

--- index.php
+++ index.php
@@ -22,7 +22,7 @@ session_start();
 	}
 ?>
 <!DOCTYPE html>
-<html lang="en">
+<html class="wf-montserrat-n7-active wf-montserrat-n4-active wf-montserrat-n5-active wf-montserrat-n6-active wf-montserrat-n8-active wf-montserrat-n9-active wf-active" lang="fr">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -35,24 +35,26 @@ session_start();
     <header>  
         <h1> Bienvenue sur le Quiz qui vous fait perdre du fric</h1>     
     </header>
-    <nav class="box">	
+    <nav>	
         <div class="controller__AppWrapper-sc-1m4rw0k-0 test">
             <div class="background__Background-sc-15eg2v3-0 test1">
                 <aside class="background-shapes__CircleShape-waao7z-1 gVOCQO"></aside>
                 <aside class="background-shapes__CircleShape-waao7z-1 eanPtI"></aside>
-                <form class="form" method="post" action="attente.php">
-                    <div>
-                        <label for="pseudo">Votre pseudo :</label>
-                        <input id="pseudo" class="champs" placeholder="Pseudo" name="pseudo" maxlength="20">
-                    </div>
-                    <div>
-                        <label for="pin">Le code pin :</label>
-                        <input name="pin" id="pin" class="champs" placeholder="Code Pin" maxlength="10">
-                    </div>
-                    <div class="button" id="submit">
-                        <input name="start" type="submit" value="Valider">
-                    </div>
-                </form>
+                <div class="box">
+                    <form class="form" method="post" action="attente.php">
+                        <div>
+                            <label for="pseudo">Votre pseudo :</label>
+                            <input id="pseudo" class="champs" placeholder="Pseudo" name="pseudo" maxlength="20">
+                        </div>
+                        <div>
+                            <label for="pin">Le code pin :</label>
+                            <input name="pin" id="pin" class="champs" placeholder="Code Pin" maxlength="10">
+                        </div>
+                        <div class="button" id="submit">
+                            <input name="start" type="submit" value="Valider">
+                        </div>
+                    </form>
+                </div>
             </div>
         </div>    
     </nav>    
