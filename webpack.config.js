40c50c768456a91817198dfcd7675f9c4af7c2ee
root <root@PORT202105-018.localdomain>
2021-07-21 09:19:09 +0200

    Fricoot

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,51 @@
 const path = require('path')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CssMinimizerPlugin = require('css-minimizer-webpack-plugin');
 
 module.exports = {
-    entry: './assets/js/app.js',
+    entry: './src/index.js',
     watch: true,
-
     output: {
         path: path.resolve('./dist'),
             filename: 'main.js'
-    }
-
-}
+    },
+    module: {
+        rules:[
+            {
+                test:/\.js$/,
+                exclude: /(node_modules|bower_components)/,
+                use:{
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env']
+                    }
+                }
+            },
+            {
+                test: /\.css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader"],
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                  // Creates `style` nodes from JS strings
+                  "style-loader",
+                  // Translates CSS into CommonJS
+                  "css-loader",
+                  // Compiles Sass to CSS
+                  "sass-loader",
+                ],
+            },
+            {
+                test: /.s?css$/,
+                use: [MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'],
+            },            
+        ],
+        
+    },
+    plugins: [
+        new MiniCssExtractPlugin({
+            filename: 'style.css',           
+        }),
+    ],
+};
