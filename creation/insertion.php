59831ad55950b67fa93283bdf704620adadcafb5
root <root@PORT202105-018.localdomain>
2021-07-27 09:35:50 +0200

    [update] fin php questionnaire

--- creation/insertion.php
+++ creation/insertion.php
@@ -4,7 +4,7 @@ session_start();
 require '../Connexion.php';
 
 $dbh = (new Connexion())->conect();
-var_dump($_POST);
+
 $question = $_POST['question_name'];
 $r = $_POST['reponse_1'];
 $r1 = $_POST['reponse_2'];
@@ -13,39 +13,41 @@ $r3 = $_POST['reponse_4'];
 $answer = $_POST['reponse_bonne'];
 $points = $_POST['points'];
 $nmbquestion = count($question);
+$err = false;
 
 for ($i = 0; $i < $nmbquestion; ++$i){
 
-    $question = $question[$i];
-    $r = $r[$i];
-    $r1 = $r1[$i];
-    $r2 = $r2[$i];
-    $r3 = $r3[$i];
-    $answer = $answer[$i];
-    $points = $points[$i];
+    $questions = $question[$i];
+    $rep = $r[$i];
+    $rep1 = $r1[$i];
+    $rep2 = $r2[$i];
+    $rep3 = $r3[$i];
+    $rb = $answer[$i];
+    $point = $points[$i];
+
+    if ( !empty($questions) && !empty($rep) && !empty($rep1) && !empty($rep2) && !empty($rep3) && !empty($rb) && !empty($point)){
+        // préparation de la requête d'insertion SQL
+            $pdoStat = $dbh->prepare('INSERT INTO questions(question_name, reponses_1, reponses_2, reponses_3, reponses_4, reponse_bonne, points) VALUES (:question_name, :reponse_1, :reponse_2, :reponse_3, :reponse_4, :reponse_bonne, :points)');
+            //on lie chaque marqueur à une valeur
+            $pdoStat->bindParam(':question_name', $questions);
+            $pdoStat->bindParam(':reponse_1', $rep);
+            $pdoStat->bindParam(':reponse_2', $rep1);
+            $pdoStat->bindParam(':reponse_3', $rep2);
+            $pdoStat->bindParam(':reponse_4', $rep3);
+            $pdoStat->bindParam(':reponse_bonne', $rb);
+            $pdoStat->bindParam(':points', $point);
+        
+            // éxécution de la requête préparée
+            $pdoStat->execute();              
+     } else {
+         $err = true;
+     }
 }
 
-if ( !empty($question) && !empty($r) && !empty($r1) && !empty($r2) && !empty($r3) && !empty($answer) && !empty($points)){
-// préparation de la requête d'insertion SQL
-    $pdoStat = $dbh->prepare('INSERT INTO questions(question_name, reponses_1, reponses_2, reponses_3, reponses_4, reponse_bonne, points) VALUES (:question_name, :reponse_1, :reponse_2, :reponse_3, :reponse_4, :reponse_bonne, :points)');
-    //on lie chaque marqueur à une valeur
-    $pdoStat->bindParam(':question_name', $question);
-    $pdoStat->bindParam(':reponse_1', $r);
-    $pdoStat->bindParam(':reponse_2', $r1);
-    $pdoStat->bindParam(':reponse_3', $r2);
-    $pdoStat->bindParam(':reponse_4', $r3);
-    $pdoStat->bindParam(':reponse_bonne', $answer);
-    $pdoStat->bindParam(':points', $points);
-
-    // éxécution de la requête préparée
-if ($pdoStat->execute() === true) {
-  
-    header('Location:../attente.php');
-    die;
-
-} else {
-
+if ($err === true){
     header('Location:./index.php');
-    die;
-    } 
+} else {
+    header('Location:../attente.php');
 }
+
+
