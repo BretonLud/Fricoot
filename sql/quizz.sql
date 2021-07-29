dedf1c70d15640dafa00eb8b56b036e7afdec8ec
root <root@PORT202105-018.localdomain>
2021-07-29 14:56:38 +0200

    [update] data

--- sql/quizz.sql
+++ sql/quizz.sql
@@ -1,137 +1,118 @@
--- phpMyAdmin SQL Dump
--- version 4.9.5deb2
--- https://www.phpmyadmin.net/
+-- MySQL dump 10.13  Distrib 8.0.26, for Linux (x86_64)
 --
--- Hôte : localhost:3306
--- Généré le : mer. 28 juil. 2021 à 15:51
--- Version du serveur :  10.3.29-MariaDB-0ubuntu0.20.04.1
--- Version de PHP : 7.4.3
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
+-- Host: 127.0.0.1    Database: quizz
+-- ------------------------------------------------------
+-- Server version	8.0.26-0ubuntu0.20.04.2
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+/*!50503 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Base de données : `quizz`
---
-CREATE DATABASE IF NOT EXISTS `quizz` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
-USE `quizz`;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `questions`
+-- Table structure for table `questions`
 --
 
+DROP TABLE IF EXISTS `questions`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `questions` (
-  `id` int(11) NOT NULL,
-  `question` varchar(250) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id` int NOT NULL AUTO_INCREMENT,
+  `question` varchar(250) NOT NULL,
+  `points` int NOT NULL,
+  `id_quiz` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=9 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `questions`
+-- Dumping data for table `questions`
 --
 
-INSERT INTO `questions` (`id`, `question`) VALUES
-(1, 'Capital de la France'),
-(2, 'Couleur du cheval blanc d\'henri 4');
-
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `questions` DISABLE KEYS */;
+INSERT INTO `questions` VALUES (1,'test1',1,1),(2,'test2',2,1),(3,'test3',3,1),(4,'test4',4,1),(5,'Suis je gay ?',100,2),(6,'Ludo Lhorents a une voiture pourri ? ',50,2),(7,'La seat est de couleur ?',100,2),(8,'Vincent est le copain de Maxime ? ',100,2);
+/*!40000 ALTER TABLE `questions` ENABLE KEYS */;
 
 --
--- Structure de la table `quiz`
+-- Table structure for table `quiz`
 --
 
+DROP TABLE IF EXISTS `quiz`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `quiz` (
-  `id` int(11) NOT NULL,
-  `nom` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
+  `id` int NOT NULL AUTO_INCREMENT,
+  `nom` text,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Structure de la table `reponses`
+-- Dumping data for table `quiz`
 --
 
+/*!40000 ALTER TABLE `quiz` DISABLE KEYS */;
+INSERT INTO `quiz` VALUES (1,'Ludotest'),(2,'maxime et Ludo');
+/*!40000 ALTER TABLE `quiz` ENABLE KEYS */;
+
+--
+-- Table structure for table `reponses`
+--
+
+DROP TABLE IF EXISTS `reponses`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `reponses` (
-  `id_question` int(11) NOT NULL,
-  `reponse_bonne` tinyint(4) NOT NULL,
-  `reponse` text DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `id_question` int NOT NULL,
+  `reponse_bonne` tinyint NOT NULL,
+  `reponse` text
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Déchargement des données de la table `reponses`
+-- Dumping data for table `reponses`
 --
 
-INSERT INTO `reponses` (`id_question`, `reponse_bonne`, `reponse`) VALUES
-(1, 2, NULL),
-(2, 3, NULL);
-
--- --------------------------------------------------------
+/*!40000 ALTER TABLE `reponses` DISABLE KEYS */;
+INSERT INTO `reponses` VALUES (1,1,'1'),(1,0,'1'),(1,0,'1'),(1,0,'1'),(2,0,'2'),(2,1,'2'),(2,0,'2'),(2,0,'2'),(3,0,'3'),(3,0,'3'),(3,1,'3'),(3,0,'3'),(4,0,'4'),(4,0,'4'),(4,0,'4'),(4,1,'4'),(5,0,'Oui'),(5,0,'Yes'),(5,0,'Yeah'),(5,1,'OFC'),(6,0,'oui'),(6,0,'Yes'),(6,1,'Yeah'),(6,0,'OFC'),(7,0,'Rouge'),(7,0,'Gris'),(7,0,'Bleu'),(7,1,'Noir'),(8,0,'Oui'),(8,1,'Yes'),(8,0,'Yeah'),(8,0,'OFC');
+/*!40000 ALTER TABLE `reponses` ENABLE KEYS */;
 
 --
--- Structure de la table `users`
+-- Table structure for table `users`
 --
 
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `id` int NOT NULL AUTO_INCREMENT,
   `pseudo` varchar(120) NOT NULL,
-  `points` int(255) NOT NULL,
-  `code_pin` int(150) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `points` int NOT NULL,
+  `code_pin` int NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=34 DEFAULT CHARSET=utf8mb3;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Index pour les tables déchargées
+-- Dumping data for table `users`
 --
 
---
--- Index pour la table `questions`
---
-ALTER TABLE `questions`
-  ADD PRIMARY KEY (`id`);
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
---
--- Index pour la table `quiz`
---
-ALTER TABLE `quiz`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `questions`
---
-ALTER TABLE `questions`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT pour la table `quiz`
---
-ALTER TABLE `quiz`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
-COMMIT;
-
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2021-07-29 14:55:35
